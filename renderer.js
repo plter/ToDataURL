c4f38103581a593ca01044674d398f666025d6a5
plter <xtiqin@163.com>
2019-03-22 21:35:38 +0800

    使用vue重构项目

--- renderer.js
+++ renderer.js
@@ -4,14 +4,56 @@
 
 window.$ = window.jQuery = require("jquery");
 require("bootstrap");
+const Vue = require("vue/dist/vue");
 
-$("#file-filed").change(e => {
-    if (e.target.files && e.target.files.length) {
-        let f = e.target.files[0];
+let Main = {
+    init() {
+        this.initUI();
+        this.addListeners();
+    },
+
+    initUI() {
+        this.root = document.querySelector("#root");
+        this.resizeByWindow();
+
+        this.initVueApp();
+
+        this.fileField = document.querySelector("#file-filed");
+    },
+
+    initVueApp() {
+        let self = this;
+        this.vueApp = new Vue({
+            el: "#root", data: {output: ""}, methods: {
+                btnOpenFileClicked: function () {
+                    self.fileField.click();
+                },
+                fileFieldChangeHandler: function (event) {
+                    if (event.target.files && event.target.files.length) {
+                        self._currentFile = event.target.files[0];
+                        self.readAndShowFileData(self._currentFile);
+                    }
+                }
+            }
+        });
+    },
+
+    readAndShowFileData(file) {
         let reader = new FileReader();
-        reader.onload = function () {
-            $("#output").val(reader.result);
+        reader.onload = () => {
+            this.vueApp.output = reader.result;
         };
-        reader.readAsDataURL(f);
+        reader.readAsDataURL(file);
+    },
+
+    addListeners() {
+        window.onresize = this.resizeByWindow.bind(this);
+    },
+
+    resizeByWindow() {
+        this.root.style.height = `${window.innerHeight}px`;
     }
-});
+};
+
+
+Main.init();
